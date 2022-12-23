6a6481ba946affb308bc20a48c6fd6b14a365692
Albert Mukiria <albert.mukiria@gmail.com>
2022-12-23 09:30:44 +0300

    Added Oscillator Script

---
+++ Assets/Scripts/Oscillator.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Oscillator : MonoBehaviour
+{
+    Vector3 startingPosition;
+    [SerializeField] Vector3 movementVector;
+    float movementFactor;
+    [SerializeField] float period;
+    void Start()
+    {
+        startingPosition = transform.position;
+        Debug.Log(startingPosition);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(period <= Mathf.Epsilon) { return; }
+
+        float cycles = Time.time / period; // continually growing over time
+        
+        const float tao = Mathf.PI * 2; //constant value of 6.283
+        float rawSinWave = Mathf.Sin(cycles * tao); //going from -1 to 1
+
+        movementFactor = (rawSinWave + 1f) / 2f; //recalculated to go from 0 to 1
+
+        Vector3 offset = movementVector * movementFactor;
+        transform.position = startingPosition + offset;
+         
+    }
+}
